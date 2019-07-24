c035da788de4d540cd8613a3c2699ada1d0cae72
BuildTools <unconfigured@null.spigotmc.org>
2019-07-24 15:14:31 -0700

    day 3

--- game.py
+++ game.py
@@ -6,11 +6,11 @@ screen_info = pygame.display.Info()
 size = (width, height) = (800, 600)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-color = (0, 127, 255)
+color = (150, 127, 0)
 
 fish = pygame.image.load("fish.png")
 image_rect = fish.get_rect()
-speed = pygame.math.Vector2(65, 53)
+speed = pygame.math.Vector2(20, 20)
 
 
 def main():
@@ -24,12 +24,15 @@ def main():
 
 def move():
     image_rect.move_ip(speed)
-    width = screen_info.current_w
     if image_rect.right > width:
         speed.x *= -1
-    if image_rect.left< 0:
-        speed.x*=-1
-    if image_rect.top< -height:
-        speed.y*= -1
-if __name__ == "main":
+    if image_rect.left < 0:
+        speed.x *= -1
+    if image_rect.bottom > height:
+        speed.y *= -1
+    if image_rect.top < 0:
+        speed.y *= -1
+
+
+if __name__ == "__main__":
     main()
